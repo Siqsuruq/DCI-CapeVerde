73eaeaf7614314c4637091ff8d864819cda31f82
Siqsuruq <siqsuruq@gmail.com>
2018-10-12 09:36:44 -0100

    Category group and subgroup
    Added category, group and subgroup name of drugs

--- create_table.sql
+++ create_table.sql
@@ -6,6 +6,9 @@ CREATE TABLE "public"."dcicv" (
 	"name" Text COLLATE "pg_catalog"."default" NOT NULL,
 	"doze" Text COLLATE "pg_catalog"."default" NOT NULL,
 	"dcicv" Text COLLATE "pg_catalog"."default" NOT NULL,
+	"categoria" Text COLLATE "pg_catalog"."default",
+	"grupo" Text COLLATE "pg_catalog"."default",
+	"subgrupo" Text COLLATE "pg_catalog"."default",
 	PRIMARY KEY ( "id" ),
 	CONSTRAINT "unique_dcicv_id" UNIQUE( "id" ) );
  ;
